b7a3314f773bde47987db2ac0d797988b0d39f8e
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-20 23:24:17 -0500

    add focus State to hide keyboard, calculate total value to pay for every

--- Project-1-SwiftUI-we-split/ContentView.swift
+++ Project-1-SwiftUI-we-split/ContentView.swift
@@ -13,9 +13,22 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
+    @FocusState var amountIsFocused: Bool
+    
     let tipPercentages = [10, 15, 20, 25, 0]
     
     let currencyFormat = Locale.current.currency?.identifier ?? "USD"
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        print(checkAmount / 100)
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+    
+        return amountPerPerson
+    }
 
     
     var body: some View {
@@ -25,6 +38,7 @@ struct ContentView: View {
                     Text("Local Currency: \(currencyFormat)")
                     TextField("Amount", value: $checkAmount, format: .currency(code: currencyFormat))
                         .keyboardType(.numberPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100, id: \.self){
@@ -34,12 +48,31 @@ struct ContentView: View {
                     .pickerStyle(.navigationLink)
                 }
                 
-                Section("Resume"){
-                    Text(checkAmount, format: .currency(code: currencyFormat))
-                    Text("Number of people \(numberOfPeople)")
+                Section("Tip percentage"){
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
+                
+                
+                Section("Resume"){
+                    Text(totalPerPerson, format: .currency(code: currencyFormat))
+                }
+                
+
             }
             .navigationTitle("WeSplit")
+            .toolbar{
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+            .toolbarRole(.editor)
             .navigationBarTitleDisplayMode(.inline)
         }
         
